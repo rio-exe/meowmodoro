66e064e7c41065b8800739becdfd5a43649ed9a1
Rio <104647278+rio-exe@users.noreply.github.com>
2025-07-20 21:29:31 -0300

    Components system created and working successfully. (#5)

--- app/system/view/View.js
+++ app/system/view/View.js
@@ -14,21 +14,11 @@ export default class View {
         fetch(this.html).then(response => response.text().then((result) => {
             document.getElementById("app").innerHTML = result;
 
-            const linkElements = document.getElementsByTagName("link");
-            Array.from(linkElements).forEach((element) => {
-                if (element.getAttribute("module-endpoint") && element.getAttribute("module-endpoint") !== window.location.pathname) {
-                    element.remove();
-                }
-            });
-            
-            if (this.style) {
-                const cssLinkElement = document.createElement("link");
-                cssLinkElement.setAttribute("rel", "stylesheet");
-                cssLinkElement.setAttribute("href", this.style);
-                cssLinkElement.setAttribute("module-endpoint", window.location.pathname);
-      
-                document.head.append(cssLinkElement);
-            }
+            // CHECK FOR COMPONENTS AND THEN LOAD THEM
+            this.loadComponents();
+
+            // LOAD STYLE
+            this.loadStyle();
 
         }).then(() => {
             this.runController();
@@ -51,6 +41,73 @@ export default class View {
 
     runController() {
         this.controller.run();
+    }
+
+    loadStyle() {
+
+        const linkElements = document.getElementsByTagName("link");
+        Array.from(linkElements).forEach((element) => {
+            if (element.getAttribute("component") || element.getAttribute("module-endpoint") && element.getAttribute("module-endpoint") !== window.location.pathname) {
+                element.remove();
+            }
+        });
+            
+        if (this.style) {
+            const cssLinkElement = document.createElement("link");
+            cssLinkElement.setAttribute("rel", "stylesheet");
+            cssLinkElement.setAttribute("href", this.style);
+            cssLinkElement.setAttribute("module-endpoint", window.location.pathname);
+      
+            document.head.append(cssLinkElement);
+        }
+    }
+
+    loadComponents() {
+        const componentElements = Array.from(document.querySelector("body").querySelectorAll("*")).filter((element) => {
+            return element.tagName.toLowerCase().includes("-component");
+        });
+        
+        componentElements.forEach((element) => {
+
+            // LOAD COMPONENT HTML INTO COMPONENT TAG
+            const componentName = element.tagName.toLowerCase();
+            import("/app/components/"+componentName+"/"+componentName+".js").then((component) => {
+                const componentController = new component.default();
+
+                // EXECUTE COOMPONENT'S CONTROLLER
+                fetch(componentController.html).then(response => response.text().then((result) => {
+                    element.innerHTML = result;
+                }).catch((textError) => {
+                    // ERROR WHILE READING FILE
+                    console.error("An error occurred while getting file: " + this.html + ". Details: " + textError);
+                }).catch((fetchError) => {
+                    // ERROR WHILE GETTING FILE
+                    console.error("An error occurred while getting file: " + this.html + ". Details: " + fetchError);
+                }));
+                
+                // CLEAR AND RUN CSS STYLES
+                const linkElements = document.getElementsByTagName("link");
+                Array.from(linkElements).forEach((element) => {
+                    if (element.getAttribute("component")) {
+                        element.remove();
+                    }
+                });
+                    
+                if (componentController.style) {
+                    const cssLinkElement = document.createElement("link");
+                    cssLinkElement.setAttribute("rel", "stylesheet");
+                    cssLinkElement.setAttribute("href", componentController.style);
+                    cssLinkElement.setAttribute("component", componentName);
+            
+                    document.head.append(cssLinkElement);
+                }
+
+                componentController.run();
+            }).catch((e) => {
+                console.error("An error ocurred while searching for " + componentName + " component. It could not be imported!");
+                throw e;
+            });
+        });
     }
 
 }
