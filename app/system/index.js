944848eb78c3ddd7eaeddc2a31304ec88ccc53b4
Riyo <flavioed7ard9@gmail.com>
2025-07-20 21:50:52 -0300

    Initial framework structure finished.

--- app/system/index.js
+++ app/system/index.js
@@ -1,14 +1,7 @@
 import Router from "/app/system/router/Router.js";
-import PomodoroController from "/app/modules/pomodoro/PomodoroController.js";
 import DefaultScreenController from "/app/modules/defaultScreen/DefaultScreenController.js";
-import StoreController from "/app/modules/store/StoreController.js";
-import StatisticsController from "/app/modules/statistics/StatisticsController.js";
 
 const router = Router.RouterInstance;
-
-router.set("/", PomodoroController);
-router.set("/store", StoreController);
-router.set("/statistics", StatisticsController);
 
 router.setDefault(DefaultScreenController);
 
