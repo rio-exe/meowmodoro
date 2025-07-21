d93749e5c026cd5e3237a77ad90523ded3838e74
Riyo <flavioed7ard9@gmail.com>
2025-07-20 21:20:05 -0300

    Components system created and working successfully.

---
+++ app/components/date-component/date-component.js
@@ -0,0 +1,12 @@
+export default class DateComponent{
+
+    constructor() {
+        this.html = "/app/components/date-component/date.html";
+        this.style = "/app/components/date-component/date.css";
+    }
+
+    run() {
+
+    }
+
+}
